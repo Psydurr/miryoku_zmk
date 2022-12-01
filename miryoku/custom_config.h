27f0fdaf70a2378b7441104c6c30893101ac055d
Psydurr <psydurr@gmail.com>
2022-12-01 15:12:14 -0500

    Adjust custom config

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -11,8 +11,8 @@
      K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
      N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
 ) \
-&kp F2  K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  &kp EQL\
-&kp F4  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  &kp SEMI \
+&kp F4  K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  &kp EQL\
+&kp F2  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  &kp SEMI \
 &kp BSLH  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  &kp MINUS \
                K32  K33  K34       K35  K36  K37
 
